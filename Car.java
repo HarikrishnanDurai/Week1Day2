34db827518ca9b7da5460999327de4ff31cf6f52
HarikrishnanDurai <107620245+HarikrishnanDurai@users.noreply.github.com>
2022-06-16 23:27:18 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,28 @@
+package Week1;
+
+public class Car {
+	public void	applyBreak()
+	{
+		System.out.println("Break is applied");	
+	}
+
+	public void applyGear()
+	{
+		System.out.println("Gear Change applied");
+	}
+	public void switchOnAc()
+	{
+		System.out.println("AC is turned ON");
+	}
+	public void applyAcclerate()
+	{
+		System.out.println("Acclerator applied");
+	}
+	public static void main(String[] args) {
+		Car mycar = new Car();
+		mycar.applyBreak();
+		mycar.applyGear();
+		mycar.applyAcclerate();
+		mycar.switchOnAc();
+	}
+}
